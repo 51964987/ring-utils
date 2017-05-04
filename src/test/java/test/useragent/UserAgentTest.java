c04e8a2788a789c9c264f2b8b2b7a81003e47146
ring <51964987@qq.com>
2017-05-04 18:28:54 +0800

    asdf

--- src/test/java/test/useragent/UserAgentTest.java
+++ src/test/java/test/useragent/UserAgentTest.java
@@ -2,7 +2,6 @@ package test.useragent;
 
 import org.junit.Test;
 
-import eu.bitwalker.useragentutils.DeviceType;
 import eu.bitwalker.useragentutils.UserAgent;
 
 public class UserAgentTest {
@@ -11,17 +10,17 @@ public class UserAgentTest {
 	public void parse(){
 		String userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0";
 		UserAgent ua = UserAgent.parseUserAgentString(userAgent);
-	    System.out.println(ua.getOperatingSystem().getDeviceType().);
-//	    if(ua.getOperatingSystem()!=null){
-//	    	System.out.println(ua.getOperatingSystem().getName());
-//	    	System.out.println(ua.getOperatingSystem().getGroup().getName());
-//	    }
-//	    //客户浏览器信息
-//	    if(ua.getBrowser()!=null){
-//	    	System.out.println(ua.getBrowser().getGroup().getName());
-//	    }
-//	    if (ua.getBrowserVersion() != null) {
-//	    	System.out.println(ua.getBrowserVersion().getVersion());
-//	    }
+	    System.out.println(ua.getOperatingSystem());
+	    if(ua.getOperatingSystem()!=null){
+	    	System.out.println(ua.getOperatingSystem().getName());
+	    	System.out.println(ua.getOperatingSystem().getGroup().getName());
+	    }
+	    //客户浏览器信息
+	    if(ua.getBrowser()!=null){
+	    	System.out.println(ua.getBrowser().getGroup().getName());
+	    }
+	    if (ua.getBrowserVersion() != null) {
+	    	System.out.println(ua.getBrowserVersion().getVersion());
+	    }
 	}
 }
